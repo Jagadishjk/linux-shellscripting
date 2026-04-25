87545f2eee55cfa1a665d9bdaf81b392993344e1
Jagadishjk <jagadishjk1002@gmail.com>
2026-04-25 12:14:00 +0530

    shell script

--- 02-special-variables.sh
+++ 02-special-variables.sh
@@ -1,11 +1,32 @@
 #!/bin/bash
 
-first_ARG=$1
-second_ARG=$2
+if [[ $# -eq 0 ]]
+then
+    echo "No Arguments Identified. Aborting..."
+    exit 1
+fi
+
 echo "script name is: $0"
-echo "process id: $$"
-echo "previous executed status: $?"
-echo "Number of arguments: $#"
+echo "process id for the script: $$"
+echo "Total arguments passed: $#"
+
+if [[ $# -ge 1 ]]
+then
+
+    echo "first argument is $1"
+fi
+
+if [[ $# -ge 2 ]]
+then
+
+    echo "second argument is $2"
+fi
+
+count=1
+for args in "$@"
+do
+    echo "$count argument is $args"
+    ((count++))    
+done
+
 echo "list of arguments in string: $@"
-echo "first argument is $1"
-echo "second argument is $2"
